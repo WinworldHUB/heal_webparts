2cc7d38853f5e67613e466041e3e4e94a2a0bbf6
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2025-04-03 12:28:17 +0530

    updated

---
+++ src/lib/data/dummy-practitioner.ts
@@ -0,0 +1,37 @@
+const practitioners: Practitioner[] = [
+    {
+      id: "1",
+      name: "Dr. Megan R. Ramos",
+      qualification: "BDS, MDS - Plastic And Cosmetic Surgeon, 16 Years Experience",
+      email: "info@example.com",
+      phone: "888-123-4567",
+      image: "https://example.com/images/megan-ramos.jpg",
+      practice: "Plastic And Cosmetic Surgeon",
+      description:
+        "We provide the highest quality medical care, individualized treatment by the country’s leading experts, and in the shortest amount of time. Each patient is assigned a case manager to handle all medical issues.\n\nThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don’t look even slightly believable. If you are going to use a passage.\n\nThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.",
+      link: "https://example.com/dr-megan-ramos",
+      availability: [
+        { day: "Monday - Friday", startTime: "09:00", endTime: "13:00" },
+        { day: "Saturday", startTime: "09:00", endTime: "17:00" },
+        { day: "Sunday", status: "Not working" },
+      ],
+    },
+    {
+      id: "2",
+      name: "Dr. John A. Carter",
+      qualification: "MBBS, MD - Cardiology, 20 Years Experience",
+      email: "dr.carter@example.com",
+      phone: "777-987-6543",
+      image: "https://example.com/images/john-carter.jpg",
+      practice: "Cardiologist",
+      description:
+        "I'm a highly experienced cardiologist dedicated to providing top-notch heart care. He believes in preventive cardiology and ensures a comprehensive approach to heart health.\n\nHis expertise includes treating cardiovascular diseases, performing diagnostic procedures, and guiding patients toward a heart-healthy lifestyle.",
+      link: "https://example.com/dr-john-carter",
+      availability: [
+        { day: "Monday - Friday", startTime: "08:00", endTime: "14:00" },
+        { day: "Saturday", startTime: "10:00", endTime: "16:00" },
+        { day: "Sunday", status: "Not working" },
+      ],
+    },
+  ];
+  
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,3 +4,21 @@ type Therapy = {
   image: string;
   link: string;
 };
+
+type Availability =
+  | { day: string; startTime: string; endTime: string }
+  | { day: string; status: "Not working" };
+
+
+type Practitioner = {
+  id: string;
+  name: string;
+  qualification: string;
+  email: string;
+  phone: string;
+  image: string;
+  practice: string;
+  description: string;
+  link: string;
+  availability: Availability[];
+}
