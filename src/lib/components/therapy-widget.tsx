5b84c0a73f81193a048682dd370ef57ff120f70b
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2025-04-03 15:08:02 +0530

    update link

--- src/lib/components/therapy-widget.tsx
+++ src/lib/components/therapy-widget.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { StaticImageData } from "next/image";
+import Link from "next/link";
 import React, { FC } from "react";
 import { PiCaretRight } from "react-icons/pi";
 
@@ -21,17 +22,15 @@ const TherapyWidget: FC<TherapyWidgetProps> = ({ therapy }) => {
       </div>
       <h3 className="mt-4 text-xl font-semibold">{therapy.title}</h3>
       <p className="mt-2 text-gray-600">{therapy.description}</p>
-      <a
+      <Link
         href={therapy.link}
         className="group mt-3 w-40 mx-0 border border-[#a99870] text-[#a99870] px-4 py-2 rounded-4xl flex items-center justify-center transition-all duration-300 hover:bg-[#a99870] hover:text-white"
-        target="_blank"
-        rel="noopener noreferrer"
       >
         Learn More
         <span className="max-w-0 overflow-hidden transition-all  group-hover:max-w-fit group-hover:ml-2 duration-300 flex items-center">
           <PiCaretRight className="text-2xl text-white" size={24} />
         </span>
-      </a>
+      </Link>
     </div>
   );
 };
