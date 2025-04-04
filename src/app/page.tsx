ce9308eef8e1b4cc22233ea4264ccd357e15b910
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2025-04-04 00:56:16 -0700

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-"use client";
-import TherapyWidget from "@/lib/components/therapy-widget";
-import { therapies } from "@/lib/data/dummy_therapies";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
-
 export default function Home() {
-  const extendedTherapies = [...therapies, ...therapies];
 
   return (
     <div className="flex justify-between items-center w-full px-4 overflow-hidden">
