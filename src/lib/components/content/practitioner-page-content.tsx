b6dd68548008727d6944b847262859b6eb721d17
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2025-05-01 22:45:55 +0530

    update practitioner page

--- src/lib/components/content/practitioner-page-content.tsx
+++ src/lib/components/content/practitioner-page-content.tsx
@@ -1,24 +1,22 @@
 "use client";
 
 import HoverCard from "@/lib/components/hover-card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/lib/components/ui/carousel";
-import Autoplay from "embla-carousel-autoplay";
 import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loader from "../ui/loader";
 import usePractitioners from "../../hooks/usePractitioners";
 import { getFullName } from "../../utils/string-util";
+import Slider from "../ui/slider";
+import { useMediaQuery } from "react-responsive";
 
 const PractitionerPageContent = () => {
   const searchParams = useSearchParams();
   const speed = searchParams.get("speed");
   const delay = speed ? parseInt(speed, 10) : 5000;
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const { practitioners, loading, error, getAllPractitioners } =
     usePractitioners();
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleCardClick = (link: string) => {
     window.open(link, "_blank");
@@ -28,52 +26,47 @@ const PractitionerPageContent = () => {
     getAllPractitioners();
   }, []);
 
-  if (loading) {
+  if (loading || error) {
     return <Loader />;
   }
 
-  if (error) {
-    return <Loader />;
-  }
   return (
     <div className="flex justify-between items-center w-full px-4 overflow-hidden">
-      <Carousel
+      <Slider
         className="w-full"
-        opts={{
-          loop: true,
-          align: "start",
-        }}
-        plugins={[Autoplay({ delay })]}
+        slideTo={currentSlide}
+        delay={delay}
+        isAutoPlay={true}
+        autoHeight={false}
+        isShowPagination={false}
+        isShowNavigation={false}
+        slidesPerView={isMobile ? 1 : 3}
+        
+        onPageChange={(page) => setCurrentSlide(page)}
       >
-        <CarouselContent className="p">
-          {(practitioners ?? ([] as Practitioner[])).map((practitioner) =>
-            practitioner ? (
-              <CarouselItem key={practitioner.id} className="basis-1/3 p-0">
-                <div className="flex flex-col items-center justify-center">
-                  <HoverCard
-                    practitioner={practitioner}
-                    onCardClick={() =>
-                      handleCardClick(
-                        `https://heal-wellness.co.uk/practitioner?id=${practitioner.id}`
-                      )
-                    }
-                  />
-
-                  <div className="flex flex-col items-center justify-center mt-4">
-                    <h2>
-                      <span className="text-xl font-semibold text-[#193a5e]">
-                        {getFullName(practitioner?.firstName, practitioner?.lastName)}
-                      </span>
-                    </h2>
-                  </div>
-                </div>
-              </CarouselItem>
-            ) : (
-              <Loader />
-            )
-          )}
-        </CarouselContent>
-      </Carousel>
+        {(practitioners ?? []).map((practitioner) => (
+          <div
+            key={practitioner.id}
+            className="flex flex-col items-center justify-center"
+          >
+            <HoverCard
+              practitioner={practitioner}
+              onCardClick={() =>
+                handleCardClick(
+                  `https://heal-wellness.co.uk/practitioner?id=${practitioner.id}`
+                )
+              }
+            />
+            <div className="flex flex-col items-center justify-center mt-4">
+              <h2>
+                <span className="text-xl font-semibold text-[#193a5e]">
+                  {getFullName(practitioner.firstName, practitioner.lastName)}
+                </span>
+              </h2>
+            </div>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
